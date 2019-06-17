2a385a3f374f2210c31907d4fc9099cd11696a87
Oliver Weber <oliver@mondough.io>
2019-06-17 16:00:19 +0200

    quick and dirty implementation

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ApolloClient, { gql } from "apollo-boost";
-import { ApolloProvider, Query } from "react-apollo";
+import { ApolloProvider } from "react-apollo";
+
+import AllCharacters from "./components/allCharacters";
 
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql/"
 });
 
 function App() {
+  const [page, setPage] = useState(1);
+
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <Query
-          query={gql`
-            {
-              characters(page: 1) {
-                info {
-                  count
-                  next
-                  prev
-                  pages
-                }
-                results {
-                  name
-                  id
-                }
-              }
-            }
-          `}
-        >
-          {({
-            loading,
-            error,
-            data: { characters: { info, results } = {} }
-          }) => {
-            console.log(loading, error, info, results);
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :(</p>;
-
-            return results.map(({ name, id }) => <p key={id}>{name}</p>);
-          }}
-        </Query>
+        <AllCharacters page={page} setPage={setPage} />
       </div>
     </ApolloProvider>
   );
