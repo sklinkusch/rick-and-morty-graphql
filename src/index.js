92690f10fe04a69540f4322141352d9454af2e27
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-20 15:46:04 +0200

    changed to hashrouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import App from "./App";
 import "./index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-{/* USE BrowserRouter */ }
+{/* USE HashRouter */ }
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter hashType="noslash" basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   , document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
